02a3063974cd9df6d3cc4c4f84485178930038bf
Thomas Hartmann <40410024+Hartmann04@users.noreply.github.com>
2019-12-16 05:41:56 +0100

    .

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-const port = 3000
+const port = 3333
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
-
 app.listen(port, () => console.log(`Servidor aberto na porta => ${port}!`)); 
 // Test
