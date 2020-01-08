c7be1642fe47619515bdff47bf4461768ac09f96
Thomas Hartmann <40410024+Hartmann04@users.noreply.github.com>
2020-01-08 21:56:00 +0100

    Dlc

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ const path = require('path');
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/livia', (req, res) => res.sendFile(path.join(__dirname + '/livia.html')));
 app.listen(port, () => console.log(`Servidor aberto na porta => ${port}!`)); 
 // Test
